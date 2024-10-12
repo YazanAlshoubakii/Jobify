7dad8e1569b8056f31cfd25fe3c04324d179f249
YazanAlshoubakii <yazan.h.alshoubaki@gmail.com>
2024-10-13 00:35:58 +0300

    Landing Page Component

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,5 +1,36 @@
+import { Link } from 'react-router-dom';
+import { Logo } from '../components';
+import Wrapper from '../assets/wrappers/LandingPage';
+import main from '../assets/images/main.svg';
+
 const Landing = () => {
-  return <div>Landing Page</div>;
+  return (
+    <Wrapper>
+      <nav>
+        <Logo />
+      </nav>
+      <div className="container page">
+        <div className="info">
+          <h1>
+            Job <span>Tracking</span> App
+          </h1>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui
+            ratione dolores aliquid facere totam tempora excepturi! Mollitia
+            saepe, reprehenderit quas placeat non adipisci, debitis voluptatem
+            iure est accusamus commodi! Laborum!
+          </p>
+          <Link to="/register" className="btn register-link">
+            Register
+          </Link>
+          <Link to="/login" className="btn">
+            Login / Demo User
+          </Link>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
+    </Wrapper>
+  );
 };
 
 export default Landing;
