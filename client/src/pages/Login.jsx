9061fecd6c720514328f1dd4776c00185a017fb0
YazanAlshoubakii <yazan.h.alshoubaki@gmail.com>
2024-10-14 18:55:13 +0300

    Login Component

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,11 +1,28 @@
 import { Link } from 'react-router-dom';
-
+import { Logo, FormRow } from '../components';
+import Wrapper from '../assets/wrappers/RegisterAndLoginPage';
 const Login = () => {
   return (
-    <div>
-      <h1>Login Page</h1>
-      <Link to="/register">Register Page</Link>
-    </div>
+    <Wrapper>
+      <form className="form">
+        <Logo />
+        <h4>Login</h4>
+        <FormRow type="email" name="email" defaultValue="yazan@gmail.com" />
+        <FormRow type="password" name="password" defaultValue="123456" />
+        <button type="submit" className="btn btn-block">
+          Submit
+        </button>
+        <button type="button" className="btn btn-block">
+          Login as a guest
+        </button>
+        <p>
+          Not a member yet?
+          <Link to="/register" className="member-btn">
+            Register
+          </Link>
+        </p>
+      </form>
+    </Wrapper>
   );
 };
 
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,7 +1,6 @@
 import { Link } from 'react-router-dom';
-import { Logo } from '../components';
+import { Logo, FormRow } from '../components';
 import Wrapper from '../assets/wrappers/RegisterAndLoginPage';
-import { FormRow } from '../components';
 const Register = () => {
   return (
     <Wrapper>
