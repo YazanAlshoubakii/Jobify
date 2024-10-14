422dab028de3f0e6039b1ca8e5dd5b2ff2df0553
YazanAlshoubakii <yazan.h.alshoubaki@gmail.com>
2024-10-14 17:42:37 +0300

    Register component

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,10 +1,35 @@
 import { Link } from 'react-router-dom';
+import { Logo } from '../components';
+import Wrapper from '../assets/wrappers/RegisterAndLoginPage';
 const Register = () => {
   return (
-    <div>
-      <h1>Register Page</h1>
-      <Link to="/login">Login Page</Link>
-    </div>
+    <Wrapper>
+      <form className="form">
+        <Logo />
+        <h4>Register</h4>
+        <div className="form-row">
+          <label htmlFor="name" className="form-label">
+            <input
+              type="text"
+              id="name"
+              name="name"
+              className="form-input"
+              defaultValue="yazan"
+              required
+            />
+          </label>
+          <button type="submit" className="btn btn-block">
+            Submit
+          </button>
+          <p>
+            Already a member?
+            <Link to="/login" className="member-btn">
+              Login
+            </Link>
+          </p>
+        </div>
+      </form>
+    </Wrapper>
   );
 };
 
