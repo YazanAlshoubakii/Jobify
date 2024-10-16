58737d67bcf9ac9a5abe0861da86f7d22aaf09af
YazanAlshoubakii <yazan.h.alshoubaki@gmail.com>
2024-10-16 17:06:09 +0300

    Add Dashboard Structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -23,7 +23,17 @@ const router = createBrowserRouter([
       { index: true, element: <Landing /> },
       { path: '/register', element: <Register /> },
       { path: '/login', element: <Login /> },
-      { path: '/dashboard', element: <DashboardLayout /> },
+      {
+        path: '/dashboard',
+        element: <DashboardLayout />,
+        children: [
+          { index: true, element: <AddJob /> },
+          { path: 'stats', element: <Stats /> },
+          { path: 'all-jobs', element: <AllJobs /> },
+          { path: 'profile', element: <Profile /> },
+          { path: 'admin', element: <Admin /> },
+        ],
+      },
     ],
   },
 ]);
