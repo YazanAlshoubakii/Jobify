ea2ae55ff290ea006070715ea9dd47b20009ee81
YazanAlshoubakii <yazan.h.alshoubaki@gmail.com>
2024-10-21 22:29:21 +0300

    Dashboard Context

--- client/src/components/BigSidebar.jsx
+++ client/src/components/BigSidebar.jsx
@@ -1,5 +1,9 @@
 import Wrapper from '../assets/wrappers/BigSidebar';
+import { useDashboardContext } from '../pages/DashboardLayout';
 const BigSidebar = () => {
+  const data = useDashboardContext();
+  console.log(data);
+
   return <Wrapper>BigSideBar</Wrapper>;
 };
 
